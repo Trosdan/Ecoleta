5f173973f4245fa02b0b6cacb1ce16e95e3de604
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-06-05 23:30:10 -0300

    Initial web files

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <Header title="Hello World" />
+      <h1>Conteúdo da aplicação</h1>
+    </div>
+  );
+}
+
+export default App;
